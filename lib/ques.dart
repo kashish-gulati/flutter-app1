b70c2a7e2ccf4b4b77cee9cac9e85a9e47b25e2d
Kashish Gulati <kashlati@gmail.com>
2020-02-24 11:55:33 +0530

    formatted Question class and aligned question

--- lib/ques.dart
+++ lib/ques.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
-class Question extends StatelessWidget{
-  String questionStatement;
+class Question extends StatelessWidget {
+  final String questionStatement;
   Question(this.questionStatement);
   @override
   Widget build(BuildContext context) {
-    return Text(questionStatement,style: TextStyle(fontSize: 20),);
+    return Text(
+      questionStatement,
+      style: TextStyle(fontSize: 20),
+      textAlign: TextAlign.center,
+    );
   }
 }
